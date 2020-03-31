2ac7d1c2a424973f02c5c5a5277b0da9a1f27c89
Abinayapriya <abiganesh4615@gmail.com>
2020-03-31 17:37:00 -0500

    first change from develop branch

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -11,6 +11,7 @@ public class RestAPITest {
 		System.out.println("PostJira1");
 		System.out.println("PostJira2");
 		System.out.println("PostJira3");
+		System.out.println("branch develop changes");
 	}
 	
 	@Test
