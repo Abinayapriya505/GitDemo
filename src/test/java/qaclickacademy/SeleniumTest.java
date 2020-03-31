d1420d29f417c25e42346fcc414689583ddc28b0
Abinayapriya <abiganesh4615@gmail.com>
2020-03-31 15:28:09 -0500

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("browserautomation");
+	}
+	
+	@Test
+	public void UIElements()
+	{
+		System.out.println("UIElements");
+	}
+}
